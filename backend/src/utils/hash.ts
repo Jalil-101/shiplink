c6b9b0a88dac172f7ddd7efaf2a574cc4d09be19
Jalil-101 <abdulmahama30@gmail.com>
2025-10-31 03:58:35 +0000

    Remove backend to re-add as subtree

--- backend/src/utils/hash.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt"
-
-export const hash_password = async (password: string) => {
-    return await bcrypt.hash(password, 10);
-}
-
-export const verify_password = async (password: string, hashedPassword: string) => {
-    return await bcrypt.compare(password, hashedPassword);
-}
